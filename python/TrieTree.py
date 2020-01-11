7b25bda77aad425ce4212791e32fecc9664235fa
binkesi <sjtusgn@163.com>
2020-01-11 11:42:48 +0800

    modify search edge, now can find every word.

--- python/TrieTree.py
+++ python/TrieTree.py
@@ -2,6 +2,7 @@ class TrieNode:
     def __init__(self):
         self.node = ['/']*26
         self.isEnd = False
+        self.word = ''
 
 
 class Trie:
@@ -15,6 +16,7 @@ class Trie:
             if node.node[offset] == '/':
                 node.node[offset] = TrieNode()
             node = node.node[offset]
+        node.word = str_in
         node.isEnd = True
 
     def search(self, str_sr):
@@ -25,20 +27,29 @@ class Trie:
                 print('No such string. {}'.format(str_sr))
                 return
             node = node.node[offset]
+        word_list = []
+        self.visit_node(node, word_list)
+        return word_list
+
+    def visit_node(self, node, word_list):
         if node.isEnd is True:
-            print('Complete.')
-            return str_sr
-        else:
-            print('Not complete.')
-            return str_sr
+            word_list.append(node.word)
+        hav_aft = 0
+        for i in node.node:
+            if i != '/':
+                self.visit_node(i, word_list)
+                hav_aft = 1
+        if hav_aft == 0:
+            return
 
 
 if __name__ == "__main__":
     try_trie = Trie()
     try_trie.insert('hello')
     try_trie.insert('her')
+    try_trie.insert('hery')
     try_trie.insert('take')
     try_trie.insert('tap')
-    try_trie.search('her')
-    try_trie.search('he')
+    print(try_trie.search('her'))
+    #try_trie.search('he')
     try_trie.search('his')
