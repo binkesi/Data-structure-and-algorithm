bbb4256f8b60e9a34ef2af1a4e990e15b64686f4
binkesi <sjtusgn@163.com>
2020-10-09 16:18:22 +0800

    add dp method

--- python/dynamic_programming/lwst_dist.py
+++ python/dynamic_programming/lwst_dist.py
@@ -16,9 +16,24 @@ def lwst_dist(a, b, init_dist):
         lwst_dist(a, b[1:], init_dist+1)
         lwst_dist(a[1:], b[1:], init_dist+1)
         
+def lwst_dist_dp(a, b):
+    dp = [[0]*len(a) for _ in range(len(b))]
+    for i in range(len(a)):
+        dp[0][i] = i
+    for j in range(len(b)):
+        dp[j][0] = j
+    for i in range(1, len(a)):
+        for j in range(1, len(b)):
+            if a[i] == b[j]:
+                dp[i][j] = min(dp[i-1][j]+1, dp[i][j-1]+1, dp[i-1][j-1])
+            else:
+                dp[i][j] = min(dp[i-1][j]+1, dp[i][j-1]+1, dp[i-1][j-1]+1)
+    return dp[-1][-1]
+        
 if __name__ == "__main__":
     a = "mitcmux"
     b = "mtacnux"
+    print(lwst_dist_dp(a, b))    
     a += 'k'
     b += 'k'
     lwst_dist(a, b, 0)
