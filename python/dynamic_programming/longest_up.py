826c2bdf3f1e4858d3141c6e7c87f32873021ea8
ecnusgn <sjtusgn@163.com>
2020-10-11 16:00:12 +0800

    output up serial

--- python/dynamic_programming/longest_up.py
+++ python/dynamic_programming/longest_up.py
@@ -4,7 +4,18 @@ def longest_up(ser):
         for k in range(i):
             if ser[i] > ser[k]:
                 dp[i] = max(dp[i], dp[k]+1)
-    return max(dp)
+    print(dp)
+    lis = []
+    max_len = max(dp)
+    for i in range(len(dp)-1, -1, -1):
+        if max_len == 0:
+            break
+        if dp[i] == max_len:
+            lis.append(ser[i])
+            max_len -= 1
+    lis.reverse()
+    return lis
+    
     
 if __name__ == "__main__":
     ser = [2, 5, 4, 3, 7, 9, 1, 3, 12, 13]
