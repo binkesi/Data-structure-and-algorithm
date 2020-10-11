9928074afd893ad54bcd5e1ba4dae8c71ebbb2b8
ecnusgn <sjtusgn@163.com>
2020-10-11 10:20:35 +0800

    self code review of lwst dist

--- python/dynamic_programming/lwst_dist.py
+++ python/dynamic_programming/lwst_dist.py
@@ -21,8 +21,9 @@ def lwst_dist_dp(a, b):
     for i in range(len(a)):
         if a[i] == b[0]:
             dp[0][i] = i
+        # when i == 0, will use dp[0][-1], should notice this situation, anyway its value is right.
         else:
-            dp[0][i] = dp[0][i-1]+1
+            dp[0][i] = dp[0][i-1] + 1
     for j in range(len(b)):
         if b[j] == a[0]:
             dp[j][0] = j
@@ -37,7 +38,7 @@ def lwst_dist_dp(a, b):
     return dp[-1][-1]
         
 if __name__ == "__main__":
-    a = "mitcmux"
+    a = "aitcmux"
     b = "mtacnux"
     print(lwst_dist_dp(a, b))    
     a += 'k'
